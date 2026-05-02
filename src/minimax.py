416b25445a98869ec16557a6beaace414560d6f9
Conor <clmkxr@umsystem.edu>
2026-05-02 18:51:15 -0500

    its better but not done yet

--- src/minimax.py
+++ src/minimax.py
@@ -2,11 +2,13 @@ class MinimaxAgent:
     def __init__(self, max_depth=7):
         self.max_depth = max_depth
 
+    def map_value(self, a, b):
+        return a - b
     def eval(self, team_a_strengths, team_b_strengths, picked_maps):
         """
         Heuristic eval for map set
         """
-        score = 0
+        
         a_wins = 0
         b_wins = 0
 
@@ -15,17 +17,12 @@ class MinimaxAgent:
             a= team_a_strengths.get(map_name, 0)
             b = team_b_strengths.get(map_name, 0)
 
-            weight = 1
-            if i in [0,1,4,5]:  
-                weight = 3
-            
-
             if a > b:
                 a_wins += 1
             elif b > a:
                 b_wins += 1
-        score += (a_wins - b_wins) * weight
-        return score
+
+        return  a_wins - b_wins
 
     def minimax(
         self,
@@ -35,21 +32,20 @@ class MinimaxAgent:
         action_index,
         picked_maps,
     ):
-
         # terminal
-        if action_index ==7:
+        if action_index == 7:
             return self.eval(team_a_strengths, team_b_strengths, picked_maps), None
-
-        if action_index in [0, 4]:  # A bans
+        
+        # Action indices: 0=A_ban1, 1=B_ban1, 2=A_pick1, 3=B_pick1, 4=A_ban2, 5=B_ban2, 6=decider
+        
+        if action_index in [0, 4]:  # A bans - A wants to ban maps that are BAD for A (negative values), so MAXIMIZE
+            maximizing = True
+        elif action_index in [1, 5]:  # B bans - B wants to ban maps BAD for B (positive for A), so from A's perspective MINIMIZE
             maximizing = False
-        elif action_index in [1, 5]:  # B bans
+        elif action_index in [2, 6]:  # A picks - A wants maps GOOD for A (positive values), so MAXIMIZE
             maximizing = True
-        elif action_index in [2]:  # A picks
-            maximizing = True
-        elif action_index in [3]:  # B picks
+        elif action_index == 3:  # B picks - B wants maps GOOD for B (negative for A), so MINIMIZE from A's perspective
             maximizing = False
-        elif action_index in [6]:  # decider pick
-            maximizing = True
 
         best_map = next(iter(available_maps))
 
@@ -57,9 +53,6 @@ class MinimaxAgent:
             best_value = float("-inf")
         else:            
             best_value = float("inf")
-
-
-        
 
         for map_name in available_maps:
             next_available = available_maps.copy()
@@ -91,8 +84,6 @@ class MinimaxAgent:
                     best_value = value
                     best_map = map_name
         return best_value, best_map
-        
-            
 
     def get_action(
         self, team_a_strengths, team_b_strengths, available_maps, action_index
@@ -106,5 +97,4 @@ class MinimaxAgent:
             action_index,
             [],
         )
-
         return best_map
